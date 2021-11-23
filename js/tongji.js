42735d4fd1a74fcba32eb30c2c5116f0da495871
banznx <1741415746@qq.com>
2021-11-23 20:14:46 +0800

    添加了文本审核接口

---
+++ js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?6735cadb3b1a4b113445901aec68b688";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
